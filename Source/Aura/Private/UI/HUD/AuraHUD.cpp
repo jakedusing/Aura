ec13a1cbd5c36be9fdced09d142b0d2e8706dec0
Jake Dusing <dusingjake@gmail.com>
2024-02-09 14:11:09 -0600

    add aura hud

---
+++ Source/Aura/Private/UI/HUD/AuraHUD.cpp
@@ -0,0 +1,13 @@
+// Copyright Jake Dusing
+
+
+#include "UI/HUD/AuraHUD.h"
+#include "UI/Widget/AuraUserWidget.h"
+
+void AAuraHUD::BeginPlay()
+{
+	Super::BeginPlay();
+
+	UUserWidget* Widget = CreateWidget<UUserWidget>(GetWorld(), OverlayWidgetClass);
+	Widget->AddToViewport();
+}
