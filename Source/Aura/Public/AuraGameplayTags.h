46f6e8eed368577eac61c105d81a1b3cac25962f
Jake Dusing <dusingjake@gmail.com>
2024-02-18 16:33:11 -0600

    add AuraGameplayTags singleton

---
+++ Source/Aura/Public/AuraGameplayTags.h
@@ -0,0 +1,25 @@
+// Copyright Jake Dusing
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameplayTagContainer.h"
+
+/**
+ *  AuraGameplayTags
+ * 
+ *  Singleton containing native Gameplay Tags
+ */
+
+struct FAuraGameplayTags
+{
+public:
+	static const FAuraGameplayTags& Get() { return GameplayTags; }
+	static void InitializeNativeGameplayTags();
+
+protected:
+
+
+private:
+	static FAuraGameplayTags GameplayTags;
+};
