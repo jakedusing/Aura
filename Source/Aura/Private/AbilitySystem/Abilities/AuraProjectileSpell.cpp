887f04cba209cb0bc0ea40842ed76b216e4d558b
Jake Dusing <dusingjake@gmail.com>
2024-02-23 12:20:31 -0600

    add spawning of projectiles

---
+++ Source/Aura/Private/AbilitySystem/Abilities/AuraProjectileSpell.cpp
@@ -0,0 +1,38 @@
+// Copyright Jake Dusing
+
+
+#include "AbilitySystem/Abilities/AuraProjectileSpell.h"
+
+#include "Actor/AuraProjectile.h"
+#include "Interaction/CombatInterface.h"
+
+void UAuraProjectileSpell::ActivateAbility(const FGameplayAbilitySpecHandle Handle, const FGameplayAbilityActorInfo* ActorInfo, const FGameplayAbilityActivationInfo ActivationInfo, const FGameplayEventData* TriggerEventData)
+{
+	Super::ActivateAbility(Handle, ActorInfo, ActivationInfo, TriggerEventData);
+
+	const bool bIsServer = HasAuthority(&ActivationInfo);
+	if (!bIsServer) return;
+
+	ICombatInterface* CombatInterface = Cast<ICombatInterface>(GetAvatarActorFromActorInfo());
+	if (CombatInterface)
+	{
+		const FVector SocketLocation = CombatInterface->GetCombatSocketLocation();
+
+		FTransform SpawnTransform;
+		SpawnTransform.SetLocation(SocketLocation);
+		// TODO: Set the projectile rotation
+
+		AAuraProjectile* Projectile = GetWorld()->SpawnActorDeferred<AAuraProjectile>(
+			ProjectileClass,
+			SpawnTransform,
+			GetOwningActorFromActorInfo(),
+			Cast<APawn>(GetOwningActorFromActorInfo()),
+			ESpawnActorCollisionHandlingMethod::AlwaysSpawn);
+
+		// TODO: Give the projectile a gameplay effect spec for causing damage.
+
+		Projectile->FinishSpawning(SpawnTransform);
+	}
+
+	
+}
