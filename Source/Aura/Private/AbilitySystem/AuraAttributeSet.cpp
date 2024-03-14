138972efe3d2868d9ab9a5eaf4beb57534848873
Jake Dusing <dusingjake@gmail.com>
2024-03-14 14:56:30 -0500

    add blocked hit and critical hit to set damage text

--- Source/Aura/Private/AbilitySystem/AuraAttributeSet.cpp
+++ Source/Aura/Private/AbilitySystem/AuraAttributeSet.cpp
@@ -170,7 +170,7 @@ void UAuraAttributeSet::ShowFloatingText(const FEffectProperties& Props, float D
 		AAuraPlayerController* PC = Cast<AAuraPlayerController>(UGameplayStatics::GetPlayerController(Props.SourceCharacter, 0));
 		if (PC)
 		{
-			PC->ShowDamageNumber(Damage, Props.TargetCharacter);
+			PC->ShowDamageNumber(Damage, Props.TargetCharacter, bBlockedHit, bCriticalHit);
 		}
 	}
 }
