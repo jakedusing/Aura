c8878532786a3871f2fc16a0606809c6f0be26d3
Jake Dusing <dusingjake@gmail.com>
2024-03-28 16:09:12 -0500

    add AI controller blackboard and behavior tree

--- Source/Aura/Public/Character/AuraEnemy.h
+++ Source/Aura/Public/Character/AuraEnemy.h
@@ -11,6 +11,8 @@
 
 class UWidgetComponent;
 class UCharacterMovementComponent;
+class UBehaviorTree;
+class AAuraAIController;
 
 /**
  * 
@@ -22,6 +24,7 @@ class AURA_API AAuraEnemy : public AAuraCharacterBase, public IEnemyInterface
 	
 public:
 	AAuraEnemy();
+	virtual void PossessedBy(AController* NewController) override;
 
 	/** Enemy Interface */
 	virtual void HighlightActor() override;
@@ -64,4 +67,9 @@ protected:
 	UPROPERTY(VisibleAnywhere, BlueprintReadOnly)
 	TObjectPtr<UWidgetComponent> HealthBar;
 
+	UPROPERTY(EditAnywhere, Category = "AI")
+	TObjectPtr<UBehaviorTree> BehaviorTree;
+	
+	UPROPERTY()
+	TObjectPtr<AAuraAIController> AuraAIController;
 };
